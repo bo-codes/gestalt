aa71234ff259c74ea0fff213b0d2a592fbc63af5
elias <99310710+bo-codes@users.noreply.github.com>
2023-01-18 00:00:58 -0800

    added clearInterval for firstDelayInterval

--- src/Components/ComingSoon/ComingSoon.js
+++ src/Components/ComingSoon/ComingSoon.js
@@ -49,8 +49,8 @@ export default function ComingSoon() {
       firstDelayInterval = setInterval(() => {
         setGestaltDone(1);
       }, 400);
+      return () => clearInterval(firstDelayInterval);
     }
-    return () => clearInterval(firstDelayInterval);
   }, [timeNumber, completeText]);
 
   useEffect(() => {
