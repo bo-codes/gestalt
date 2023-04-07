817c13d03627c71d0cdf715daa4bd739b61c284c
elias <99310710+bo-codes@users.noreply.github.com>
2023-04-07 16:47:20 -0700

    added notes on TextScan and ObjectViewer components

--- src/Components/ObjectViewer/ObjectViewer.js
+++ src/Components/ObjectViewer/ObjectViewer.js
@@ -1,77 +1,124 @@
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { ObjectLoader } from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import example from "./example_3.obj";
-import example1 from "./gestalt_cube_cut_1.obj";
-import example1_2 from "./gestalt_cube_cut_1_2.obj";
-import example2 from "./gestalt_cube_cut_2.obj";
-import example3 from "./gestalt_cube_cut_3.obj";
-import blenderExample from "./gestalt_cube.obj";
-import blenderExample2 from "./gestalt_cube_2.obj";
-import blenderExample3 from "./gestalt_cube_3.obj";
 import blenderExample4 from "./gestalt_cube_4.obj";
 
-const IfcModelViewer = ({ ifcFile }) => {
+const IfcModelViewer = () => {
+  // CREATING REFERENCE TO THE DIV WHERE WE WILL SHOW OUR CANVAS
   const containerRef = useRef(null);
 
   useEffect(() => {
+    // -------------- SCENE SETUP -------------- vv
     const scene = new THREE.Scene();
+    // ADDS MAIN XYZ(RGB) AXES TO SCENE
     // scene.add(new THREE.AxesHelper(5));
+    //
+    //
+    // ------ CREATES CAMERA THAT WE WILL SEE THE SCENE THROUGH
     const camera = new THREE.PerspectiveCamera(25, 1, 0.1, 2000);
-
-    camera.position.z = -400;
-    // camera.position.x = -20;
-    // camera.position.y = -4;
-
-    // const light = new THREE.PointLight();
+    // ------ SETS POSITION OF CAMERA(DEFAULT IS (0,0,0))
+    camera.position.set(0, 0, -400);
+    // ------ THIS CODE IS ALSO ACCOMPLISHING THE SAME AS THE CODE ABOVE
+    // camera.position.z = -400;
+    //
+    //
+    // ------ CREATE AN AMBIENT LIGHT
     const ambLight = new THREE.AmbientLight(0x404040);
+    // ------ ADD THE LIGHT TO THE SCENE
     scene.add(ambLight);
-    // light.position.set(200.5, 70.5, 105);
+    // ------ CREATE A DIRECTIONAL LIGHT
     let light = new THREE.DirectionalLight();
+    // ------ ADD THE LIGHT TO THE SCENE
     scene.add(light);
+    // ------ NOT SURE WHAT THIS DOES YET
     let helper = new THREE.DirectionalLightHelper(light);
     scene.add(helper);
-
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(360, 360);
-
-    //Creates the orbit controls (to navigate the scene)
+    // -------------- SCENE SETUP -------------- ^^
+    //
+    //
+    //
+    //
+    // -------------- CREATE ORBIT CONTROLS (to navigate the scene) -------------- vv
     const controls = new OrbitControls(camera, containerRef.current);
-    // controls.addEventListener("change", () => renderer.render(scene, camera));
-    // controls.enableDamping = true;
+    // MAKES THE PHYSICS HEAVIER/NICER FOR CONTROLS
+    controls.enableDamping = true;
+    // MAKES MODEL AUTO-ROTATE
     controls.autoRotate = true;
+    // SETS THE ROTATION SPEED
     controls.autoRotateSpeed = 4;
+    // SETTING TO FALSE MAKES IT SO USER CAN'T PAN
     controls.enablePan = false;
+    // SETTING TO FALSE MAKES IT SO USER CAN'T ZOOM
     controls.enableZoom = false;
-    // controls.target.set(2, 2, 2);
-
+    // -------------- CREATE ORBIT CONTROLS (to navigate the scene) -------------- ^^
+    //
+    //
+    //
+    //
+    // -------------------- RENDERER SETUP -------------------- vv
+    // ------ FUNCTION TO CALCULATE RENDER SIZE DEPENDING ON THE WIDTH OF OUR WINDOW(CHECKS IF WE ARE ON MOBILE OR NOT)
+    const renderSize = () => {
+      if (window.innerWidth < 500) {
+        return 240;
+      }
+      return 360;
+    };
+    // ------ CREATES A NEW INSTANCE OF A RENDERER
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    // ------ SETS THE SIZE OF THE RENDERER TO THE VALUE OUR renderSize FUNCTION RETURNS
+    renderer.setSize(renderSize(), renderSize());
+    // -------------------- RENDERER SETUP -------------------- ^^
+    //
+    //
+    //
+    //
+    // renderer HAS PROPERTY domElement. THIS HOLDS OUR CANVAS WITH OUR MODEL. WE ARE ATTACHING THAT TO THE DIV WHERE WE WANT TO SHOW IT BELOW IN OUR COMPONENT RETURN.
     containerRef.current.appendChild(renderer.domElement);
-
+    //
+    //
+    //
+    //
+    // -------------------- OBJECT LOADER SETUP -------------------- vv
+    // ------ CREATES LOADER
     const objLoader = new OBJLoader();
+    // ------ LOADS THE OBJ FILE
     objLoader.load(blenderExample4, (model) => {
+      // once the obj is loaded, add it to the scene
       scene.add(model);
+      // once the obj is added to the scene, render the scene
       renderer.render(scene, camera);
     });
-
+    // -------------------- OBJECT LOADER SETUP -------------------- ^^
+    //
+    //
+    //
+    //
+    // -------------------- ANIMATE FUNCTION -------------------- vv
     const animate = () => {
+      // ------ REQUEST THE ANIMATION
       requestAnimationFrame(animate);
+      // ------ UPDATE THE CONTROLS
       controls.update();
+      // ------ THIS IS IF WE ARE USING THE LIGHT HELPER
       helper.update();
+      // ------ RENDER THE SCENE
       renderer.render(scene, camera);
     };
+    // CALL THE ANIMATE FUNCTION
     animate();
-
+    // -------------------- ANIMATE FUNCTION -------------------- ^^
+    //
+    //
+    //
+    //
     return () => {
       // containerRef.current.removeChild(renderer.domElement);
     };
-  }, [ifcFile]);
+  }, []);
 
   return (
-    <div ref={containerRef} style={{ width: "100%", height: "100%" }}>
-      {/* Your browser doesn't support WebGL. */}
-    </div>
+    <div ref={containerRef} style={{ width: "100%", height: "100%" }}></div>
   );
 };
 
--- src/Components/TextScan/TextScan.js
+++ src/Components/TextScan/TextScan.js
@@ -2,23 +2,41 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import "./TextScan.css";
 
+
+/* THE WAY THAT THIS COMPONENT WORKS IS THAT IT:
+- TAKES A BODY OF TEXT
+- SPLITS THE BODY OF TEXT INTO CHARS
+- ITERATES THROUGH ALL THE CHARS;
+    IF THE CHAR IS IN OUR 'used' OBJ, WE SHOW IT AT 100% OPACITY;
+    OTHERWISE, SHOW A RANDOM CHARACTER FROM OUR BODY OF TEXT AT 36% OPACITY
+ */
+
+// BUILT-IN STYLE FOR EACH CHARACTER
 const Character = styled.div`
-  width: 12px;
-  display: flex;
-  justify-content: center;
+width: 12px;
+display: flex;
+justify-content: center;
 `;
 
+// BUILT-IN STYLE FOR FADED CHARACTER
 const FadedCharacter = styled(Character)`
   opacity: 36%;
 `;
 
+// THIS OBJ KEEPS TRACK OF LETTERS WE ARE SHOWING
+// WE ADD LETTERS FROM OUR TEXT BODY TO IT
 let used = {};
 
-const TextScan = ({value, scanStyle}) => {
-  // actual in-order text
 
+const TextScan = ({value, scanStyle}) => {
   // in-order text split into indiv characters
   let letters = value.split("");
+
+  //  ------------------- SOLELY USED TO CREATE A RANDOMIZED PARAGRAPH OF LETTERS, THE SAME LENGTH AS OUR INTAKEN TEXT BODY ------------------- vv
+  // returns a random index number that is within the body of text we intook
+  const randomCharIndex = () => {
+    return Math.floor(Math.random() * (value.length - 0 + 1) + 0);
+  };
 
   // returns paragraph with same amount of characters as the text we are going to display, but characters are random
   let randomizedAlph = (letters) => {
@@ -26,15 +44,11 @@ const TextScan = ({value, scanStyle}) => {
     let i = 0;
     while (i < letters.length) {
       randomizedP.push(letters[randomCharIndex()]);
-      i++
+      i++;
     }
     return randomizedP.join("");
   };
-
-  // returns a random index number that is within the text we are going to display
-  const randomCharIndex = () => {
-    return Math.floor(Math.random() * (value.length - 0 + 1) + 0);
-  };
+  //  ------------------- SOLELY USED TO CREATE A RANDOMIZED PARAGRAPH OF LETTERS, THE SAME LENGTH AS OUR INTAKEN TEXT BODY ------------------- ^^
 
   // this will hold the text we will be displaying in the about-desc
   const [text, setText] = useState(randomizedAlph(letters));
@@ -43,23 +57,31 @@ const TextScan = ({value, scanStyle}) => {
   // this will keep track of how many letter switches we have/the scan effect
   const [hit8, setHit8] = useState(0);
 
+  // THIS FUNCTION WILL TAKE THE INTAKEN TEXT BODY, AND RETURNS AN ARRAY OF UNIQUE CHARACTERS
   const getUniqueChars = (str) => {
     let arr = [];
-    for (let char of str.split('')) {
+    for (let char of str.split("")) {
       if (arr.indexOf(char) < 0) {
-        arr.push(char)
+        arr.push(char);
       }
     }
-    return arr
-  }
+    return arr;
+  };
 
   useEffect(() => {
+    // set uniqueChars to equal the arr of all unique chars in our body of text
     let uniqueChars = getUniqueChars(value);
+    // FUNCTION THAT BASICALLY CAUSES THE ANIMATION BY UPDATING THE 'text' STATE OVER AND OVER
     const randomFunc = () => {
+      // THIS IS A SORT OF BASE CASE; WHEN WE HIT THIS CASE, WE ADD A NEW CHAR TO THE 'used' OBJ HENCE RENDERING A NEW CHARACTER IN ITS CORRECT PLACE
       if (hit8 === 8) {
+        // WE RESET OUR hit8 TO MAKE SURE WE WAIT A COUPLE OF RANDOM TEXT RE-RENDERS TO ADD A NEW LETTER TO THE 'used' OBJ
         setHit8(0);
+        // WE SLICE THE UNIQUE CHARS AT THE LAST CHARACTER WE'VE ADDED TO THE 'used' OBJ TO ENSURE THESE ARE ALL CHARS THAT HAVEN'T BEEN RENDERED YET
         uniqueChars = uniqueChars.slice(hitUniqueChars);
+        // WE GRAB THE FIRST LETTER IN OUR FRESHLY SLICED ARR OF UNIQUE CHARS.
         const correctLetterToSet = uniqueChars[0];
+        // WE ADD THAT UNIQUE CHAR TO THE 'used' OBJ
         used[correctLetterToSet] = 1;
       }
 
@@ -70,20 +92,20 @@ const TextScan = ({value, scanStyle}) => {
         let char = letters[i];
         // if we havent stored the current char in the object, push a random character in its place
         if (used[char.toLowerCase()] !== 1) {
-          if (scanStyle === 'digit') {
+          if (scanStyle === "digit") {
             // USING 0 AND 8 AS THE RANDOM CHARS FOR THE SCAN
-            if(i % 2 === 0) {
-              newP.push('0');
+            if (i % 2 === 0) {
+              newP.push("0");
             } else {
-              newP.push('8')
+              newP.push("8");
             }
           }
-          if (scanStyle === 'alph') {
+          if (scanStyle === "alph") {
             // USING AN ACTUAL RANDOM CHAR FROM THE TEXT AS THE RANDOM CHARS FOR THE SCAN
             newP.push(value[randomCharIndex()]);
           }
 
-          // if this character is in our object, we just push the character as it is
+          // if this character is in our object, we just push the character as it is in full opacity/render it
         } else {
           newP.push(char);
         }
@@ -91,23 +113,31 @@ const TextScan = ({value, scanStyle}) => {
 
       setText(newP.join(""));
     };
-
+    // CREATE INTERVAL
     let animationInterval;
+    // If we haven't yet hit all of our uniqueChars, set the interval;
     if (hitUniqueChars <= uniqueChars.length) {
+      // setInterval
       animationInterval = setInterval(() => {
+        // if we have hit 8 iterations of random chars/not setting/rendering a new unique char in its correct place:
         if (hit8 === 8) {
+          // increment the hitUniqueChars state because we are about to add a new unique char to the used OBJ by calling the randomFunc function
           setHitUniqueChars(hitUniqueChars + 1);
           randomFunc();
+        // otherwise, just call the randomFunc which will render no new unique characters in their respective correct place
         } else {
           randomFunc();
+          // make sure to increment the count8 so that after doing these random character renders 8 times, we finally add a new char from uniqueChars to the used OBJ and render it the next time we call the randomFunc function after that
           setHit8(hit8 + 1);
         }
       }, 2);
+      // clear the interval each time
       return () => clearInterval(animationInterval);
     }
     return () => (used = {});
   });
 
+  // this is just to make sure that our randomText we display is the same length as the input text body
   const addChars = (textLength) => {
     let addedChars = [];
     for (let i = 0; i < value.length - textLength; i++) {
@@ -119,24 +149,18 @@ const TextScan = ({value, scanStyle}) => {
   return (
     <div id="about-text">
       {text.split("").map((char, i, arr) => {
-          return used[char] ? (
-            <Character key={i}>
-              {char}
-            </Character>
-          ):(
-            <FadedCharacter key={i}>
-              {char}
-            </FadedCharacter>
-          )
-        }
-      )}
+        // if the char is stored in our used OBJ, render it with the "Character" styled div
+        return used[char] ? (
+          <Character key={i}>{char}</Character>
+          ) : (
+            // otherwise, render it with the "FadedCharacter" styled div
+            <FadedCharacter key={i}>{char}</FadedCharacter>
+        );
+      })}
+      {/* the jsx below just gives us extra characters in case the length of our random text does not match the length of the intaken text body */}
       {text.split("").length !== value.length ? (
         addChars(text.split("").length).map((char, i) => {
-          return (
-            <Character key={i}>
-              {char}
-            </Character>
-          );
+          return <Character key={i}>{char}</Character>;
         })
       ) : (
         <></>
